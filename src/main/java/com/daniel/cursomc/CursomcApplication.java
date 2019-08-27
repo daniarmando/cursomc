01adc3f3fd62b566e17620a3c96836237404c51a
daniarmando <daniel.armando@live.com>
2019-08-27 11:13:18 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/daniel/cursomc/CursomcApplication.java
+++ src/main/java/com/daniel/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.daniel.cursomc.domain.Cidade;
 import com.daniel.cursomc.domain.Cliente;
 import com.daniel.cursomc.domain.Endereco;
 import com.daniel.cursomc.domain.Estado;
+import com.daniel.cursomc.domain.ItemPedido;
 import com.daniel.cursomc.domain.Pagamento;
 import com.daniel.cursomc.domain.PagamentoComBoleto;
 import com.daniel.cursomc.domain.Pedido;
@@ -25,6 +26,7 @@ import com.daniel.cursomc.repository.CidadeRepository;
 import com.daniel.cursomc.repository.ClienteRepository;
 import com.daniel.cursomc.repository.EnderecoRepository;
 import com.daniel.cursomc.repository.EstadoRepository;
+import com.daniel.cursomc.repository.ItemPedidoRepository;
 import com.daniel.cursomc.repository.PagamentoRepository;
 import com.daniel.cursomc.repository.PedidoRepository;
 import com.daniel.cursomc.repository.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -121,6 +126,18 @@ public class CursomcApplication implements CommandLineRunner {
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }
